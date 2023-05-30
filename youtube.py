6bcb6fb3dbb930b10ce6e09be3db9b399d4a36b5
chicojnr <souza.jnr@gmail.com>
2023-05-30 19:53:09 -0400

    init

---
+++ youtube.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from chat_downloader import ChatDownloader
+import json
+
+
+app = Flask(__name__)
+
+@app.get('/')
+def index():
+    try:
+        chat = ChatDownloader().get_chat("https://www.youtube.com/watch?v=" + url)
+        chat_data = []
+        for message in chat:
+            message["author"]["images"] = message["author"]["images"][2]["url"]
+
+            # if "badges" in message["author"]:
+            #     message["author"]["badges"] = message["author"]["badges"][0]["title"]
+
+            if "emotes" in message:
+                del message["emotes"]
+
+            # if "message_type" in message:
+            #     del message["message_type"]
+
+            if "action_type" in message:
+                del message["action_type"]
+
+            chat_data.append(message)
+        return json.dumps(chat_data, ensure_ascii=False)
+    except Exception as e:
+        return json.dumps({"error": str(e)}, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+        app.run()
