a83821a2dbb1cc248d4d33cde624929645c71f8a
chicojnr <souza.jnr@gmail.com>
2023-06-02 22:50:57 -0400

    fix 0.0.3

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 @app.get('/')
 def index():
     try:
-        chat = ChatDownloader().get_chat("https://www.youtube.com/watch?v=" + request.args.get('id'), message_groups=['messages'])
+        chat = ChatDownloader().get_chat("https://www.youtube.com/watch?v=" + request.args.get('id'), message_groups=['messages', 'superchat'])
         chat_data = []
         for message in chat:
             message["author"]["images"] = message["author"]["images"][2]["url"]
 
-            # if "badges" in message["author"]:
-            #     message["author"]["badges"] = message["author"]["badges"][0]["title"]
+            if "badges" in message["author"]:
+                message["author"]["badges"] = message["author"]["badges"][0]["title"]
 
             if "emotes" in message:
                 del message["emotes"]
@@ -23,8 +23,8 @@ def index():
             # if "message_type" in message:
             #     del message["message_type"]
 
-            if "action_type" in message:
-                del message["action_type"]
+            # if "action_type" in message:
+            #     del message["action_type"]
 
             chat_data.append(message)
         return json.dumps(chat_data, ensure_ascii=False)
