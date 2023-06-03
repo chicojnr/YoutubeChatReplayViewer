048d59e78892fcbbe6f642aa7f8f1d0d315396a1
chicojnr <souza.jnr@gmail.com>
2023-06-03 15:38:01 -0400

    timeout

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,4 @@
-timeout = 60  # Tempo limite de 60 segundos
+timeout = 120  # Tempo limite de 60 segundos
 
 bind = "0.0.0.0:8000"  # Endereço e porta em que o servidor deve ser executado
 workers = 4  # Número de processos de trabalho
