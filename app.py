686652c90db81baca1d43a24a658c53d538f1431
iotwatchdog <iotwhatchdog@gmail.com>
2023-09-12 02:46:48 -0400

    CORS

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from chat_downloader import ChatDownloader
 from pytube import YouTube
 import gc
 import json
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 request_counter = 0  # Contador de requisições
 
